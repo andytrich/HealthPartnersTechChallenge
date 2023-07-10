4c39a9615f3fb220b57625e1d77a84eea5208401
Andy Rich <Andy.rich@ivcevidensia.com>
2023-07-10 17:41:33 +0100

    Remember to exclude numbers dividable by 3 and 5

--- UnitTests/FizzBuzzServiceUnitTests.cs
+++ UnitTests/FizzBuzzServiceUnitTests.cs
@@ -64,7 +64,7 @@ namespace UnitTests
         {
             for (int i = 1; i < 100; i++)
             {
-                if (i % 3 == 0) { yield return new object[] { i }; }                
+                if (i % 3 == 0 && i % 15 != 0) { yield return new object[] { i }; }
             }            
         }
     }
